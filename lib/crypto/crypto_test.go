0d4513d07a049181e205a8ed661e198a0ff6a73e
Guido Ronchetti <dyst0ni3@gmail.com>
2016-03-07 00:50:59 +0100

    [add] [crypto]
    added crypto source files and empty test files.

---
+++ lib/crypto/crypto_test.go
@@ -0,0 +1,14 @@
+//
+// 3nigm4 crypto package
+// Author: Guido Ronchetti <dyst0ni3@gmail.com>
+// v1.0 06/03/2016
+//
+package crypto
+
+import (
+	"testing"
+)
+
+func TestDerivationFunction(t *testing.T) {
+
+}
