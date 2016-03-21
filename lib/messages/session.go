2397db3f4044a7bb1366a155e20272d580c5d797
Guido Ronchetti <dyst0ni3@dyst0ni3.fedora.iks.local>
2016-03-21 19:58:22 +0100

    [upd] added functions.

--- lib/messages/session.go
+++ lib/messages/session.go
@@ -3,15 +3,18 @@
 // Author: Guido Ronchetti <dyst0ni3@gmail.com>
 // v1.0 21/03/2016
 //
-package messages
+package message
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/json"
+	"golang.org/x/crypto/openpgp"
 	"io"
 )
 
 import (
-	crypto3n "github.com/nexocrew/3nigm4/crypto"
+	crypto3n "github.com/nexocrew/3nigm4/lib/crypto"
 )
 
 // Struct that contains all required keys to
@@ -24,7 +27,8 @@ type SessionKeys struct {
 	ServerSymmetricKey []byte `json:"serverk" xml:"serverk"`       // server symmetric key;
 	PreSharedFlag      bool   `json:"presharedf" xml:"presharedf"` // is there also a pre-shared key in use;
 	PreSharedKey       []byte `json:"-" xml:"-"`                   // pre shared key (only available in the client);
-	SessionId          []byte `json:"-" xml:"-"`                   // session id returned by the server after creating the session.
+	SessionId          []byte `json:"-" xml:"-"`                   // session id returned by the server after creating the session;
+	ServerTmpKey       []byte `json:"-" xml:"-"`                   // server generated in memory key (shoul never be stored anywhere).
 }
 
 const (
@@ -33,7 +37,7 @@ const (
 
 // randomBytesForLen creates a random data blob
 // of length "size".
-func randomBytesForLen(size int) ([]byte, err) {
+func randomBytesForLen(size int) ([]byte, error) {
 	randData := make([]byte, size)
 	if _, err := io.ReadFull(rand.Reader, randData); err != nil {
 		return nil, err
@@ -46,11 +50,11 @@ func randomBytesForLen(size int) ([]byte, err) {
 func NewSessionKeys(creatorId string, preshared []byte) (*SessionKeys, error) {
 	sk := SessionKeys{}
 	var err error
-	sk.MainSymmetricKey, err = randomBytesForLen(kAesSharedKeySize)
+	sk.MainSymmetricKey, err = randomBytesForLen(kSharedKeySize)
 	if err != nil {
 		return nil, err
 	}
-	sk.ServerSymmetricKey, err = randomBytesForLen(kAesSharedKeySize)
+	sk.ServerSymmetricKey, err = randomBytesForLen(kSharedKeySize)
 	if err != nil {
 		return nil, err
 	}
@@ -65,6 +69,29 @@ func NewSessionKeys(creatorId string, preshared []byte) (*SessionKeys, error) {
 
 // encryptForRecipient creates an encrypted message to pass
 // session keys to one of the recipients.
-func (sk *SessionKeys) encryptForRecipient(kr []byte) ([]byte, error) {
+func (sk *SessionKeys) EncryptForRecipients(recipients openpgp.EntityList, signer *openpgp.Entity) ([]byte, error) {
+	// encode sessions keys
+	encoded, err := json.Marshal(sk)
+	if err != nil {
+		return nil, err
+	}
+	// encrypt encoded
+	encryped, err := crypto3n.OpenPgpEncrypt(encoded, recipients, signer)
+	if err != nil {
+		return nil, err
+	}
+	return encryped, nil
+}
 
+func (sk *SessionKeys) EncryptMessage(message []byte) ([]byte, error) {
+	mainAesKey := deriveAesKey(sk.MainSymmetricKey)
+
+}
+
+func (sk *SessionKeys) DecryptAndAbsorbeServerKey(chipered []byte) error {
+
+}
+
+func deriveAesKey(longKey []byte) [32]byte {
+	return sha256.Sum256(longKey)
 }
---
+++ lib/messages/session_test.go
@@ -0,0 +1,6 @@
+//
+// 3nigm4 crypto package
+// Author: Guido Ronchetti <dyst0ni3@gmail.com>
+// v1.0 21/03/2016
+//
+package message
